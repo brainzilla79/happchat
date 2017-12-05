713ca7fd94b2338ee67de9f3ebdd2bedabc5934a
Alex <alexanderhapp@me.com>
2017-12-05 07:50:30 -0800

    Fix typo

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
     end 
     resources :messages, only: [:create, :destroy]
   end 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details on the DSL within this file, see http://guides.rubyonrails.org/routing.html
 end
